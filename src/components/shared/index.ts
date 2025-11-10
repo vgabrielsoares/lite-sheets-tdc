09867a1debe79386d0bbf0df9a1d57f734a622c3
Victor Gabriel Soares <4vgabriel@gmail.com>
2025-11-10 11:26:01 -0300

    #20 - feat: add ConfirmDialog component with customizable props and accessibility features

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -6,3 +6,6 @@ export { Sidebar } from './Sidebar';
 export type { SidebarProps, SidebarWidth } from './Sidebar';
 
 export { default as NotificationProvider } from './NotificationProvider';
+
+export { default as ConfirmDialog } from './ConfirmDialog';
+export type { ConfirmDialogProps } from './ConfirmDialog';
