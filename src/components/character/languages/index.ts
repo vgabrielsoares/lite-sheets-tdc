d3b777152ea670fa2a86cfe5f45363c40253b47a
Victor Gabriel Soares <4vgabriel@gmail.com>
2025-12-02 11:14:55 -0300

    #38 - feat: add LanguageSelector and LanguagesDisplay components for character language management

---
+++ src/components/character/languages/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Language components
+ *
+ * Components for managing and displaying character languages
+ */
+
+export { LanguagesDisplay } from './LanguagesDisplay';
+export type { LanguagesDisplayProps } from './LanguagesDisplay';
+
+export { LanguageSelector } from './LanguageSelector';
+export type { LanguageSelectorProps } from './LanguageSelector';
