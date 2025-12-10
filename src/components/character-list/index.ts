290381105231616eee328f4fefb04aa39983f393
Victor Gabriel Soares <4vgabriel@gmail.com>
2025-12-10 13:21:07 -0300

    #88 - feat: add ImportService for importing character data from JSON files with validation

--- src/components/character-list/index.ts
+++ src/components/character-list/index.ts
@@ -6,9 +6,11 @@
  * - CharacterList: Componente principal de listagem
  * - CharacterCard: Card individual de personagem
  * - EmptyState: Estado vazio reutilizável
+ * - ImportCharacterButton: Botão para importar fichas
  */
 
 export { default } from './CharacterList';
 export { default as CharacterList } from './CharacterList';
 export { default as CharacterCard } from './CharacterCard';
 export { default as EmptyState } from './EmptyState';
+export { default as ImportCharacterButton } from './ImportCharacterButton';
